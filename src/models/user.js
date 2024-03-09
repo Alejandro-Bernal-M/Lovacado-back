bfe3348c5d95a98c255d2874e8f387b56aabb512
Alejandro Bernal Marin <alejandrober96@gmail.com>
2024-03-09 10:55:16 -0500

    add user model

---
+++ src/models/user.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 30,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 30,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+  contactNumber: {
+    type: String
+  },
+  profileImage: {
+    type: String
+  }
+  }, {timestamps: true}
+)
+
+UserSchema.virtual("password").set(function(password){
+  this.hashPassword = bcrypt.hashSync(password, 12);
+});
+
+UserSchema.virtual('fullName').get(function(){
+  return `${this.firstName} ${this.lastName}`;
+})
+
+UserSchema.methods = {
+  authenticate: function(password){
+    return bcrypt.compareSync(password, this.hashPassword );
+  }
+}
+
+module.exports = mongoose.model('User', UserSchema);
