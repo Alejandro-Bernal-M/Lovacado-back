ded091387fb77c8472a01bccd6cfd2a1ed64e419
Alejandro Bernal Marin <alejandrober96@gmail.com>
2024-03-23 13:32:15 -0500

    Add delete product method

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -30,6 +30,23 @@ exports.createProduct = async(req, res) => {
     }
   } catch (error) {
     console.log(error);
-    res.status(400).json({message: 'something went wrong', error: error})
+    res.status(500).json({message: 'Something went wrong', error: error})
   }
 }
+
+exports.deleteProduct = async (req, res) => {
+  const { productId } = req.body;
+
+  try {
+    const product = await Product.findOneAndDelete({ _id: productId }); // Find and delete the product
+
+    if (product) {
+      res.status(200).json({ message: 'Product successfully deleted', product });
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong', error: error});
+  }
+};
