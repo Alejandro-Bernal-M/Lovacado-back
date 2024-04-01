e4332a3dfa4ab6e8422f95824cf6d331e1d0a408
Alejandro Bernal Marin <alejandrober96@gmail.com>
2024-04-01 12:17:38 -0500

    Add signin method

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,40 +2,71 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken');
 
 
-exports.signup = async(req, res) => {
-  const userFoundByEmail = await User.findOne({email: req.body.email});
-  const userFoundByUsername = await User.findOne({username: req.body.username});
+exports.signup = async (req, res) => {
+  try {
+    const userFoundByEmail = await User.findOne({ email: req.body.email });
+    const userFoundByUsername = await User.findOne({ username: req.body.username });
 
-  if(userFoundByEmail || userFoundByUsername){
-    return res.status(400).json({message: 'User already exists'});
-  }else {
-    try{const {firstName, lastName, username, password, email, adminPassword, role} = req.body;
-    const newUser = new User({
-      firstName,
-      lastName,
-      username,
-      password,
-      email
-    });
-    
-    if(role){
-      if(adminPassword == process.env.ADMIN_PASSWORD){
-        newUser.role = role;
-      }else {
-        return res.status(400).json({message: "Wrong admin password"})
+    if (userFoundByEmail || userFoundByUsername) {
+      return res.status(400).json({ message: 'User already exists' });
+    } else {
+      const { firstName, lastName, username, password, email, adminPassword, role } = req.body;
+      const newUser = new User({
+        firstName,
+        lastName,
+        username,
+        password,
+        email
+      });
+
+      if (role) {
+        if (adminPassword === process.env.ADMIN_PASSWORD) {
+          newUser.role = role;
+        } else {
+          return res.status(400).json({ message: "Wrong admin password" });
+        }
+      }
+
+      const savedUser = await newUser.save();
+      if (savedUser === newUser) {
+        return res.json({ user: savedUser });
+      } else {
+        return res.status(400).json({ message: 'Error saving the user' });
       }
     }
-    const savedUser = await newUser.save();
-    if(savedUser === newUser){
-      return res.json({user: savedUser});
-    }else{
-      return res.status(400).json({message: 'Error saving the user'});
-    }
-  }catch(error){
+  } catch (error) {
     console.log(error);
-    console.log('------------------------')
-    console.log(error.errors)
-    return res.status(400).json({message: 'Something went wrong', errors: error.errors})
+    console.error(error.errors);
+    return res.status(400).json({ message: 'Something went wrong', errors: error.errors });
   }
+};
+
+exports.signin = async (req, res) => {
+  try {
+    const foundUser = await User.findOne({ email: req.body.email });
+    if (!foundUser) return res.status(400).json({ message: "Incorrect email or password" });
+
+    if (foundUser.authenticate(req.body.password)) {
+      const token = jwt.sign({ _id: foundUser._id, role: foundUser.role }, process.env.JWT_SECRET, { expiresIn: '2h' });
+      const { _id, firstName, lastName, email, role, fullName } = foundUser;
+
+      return res.status(200).json({
+        token,
+        user: {
+          _id,
+          firstName,
+          lastName,
+          email,
+          role,
+          fullName
+        }
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ message: "Something went wrong", error: error });
   }
-}
+};
