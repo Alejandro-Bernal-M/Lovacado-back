2b71aea580e78cdfd23705057122297c3280acbf
Alejandro Bernal Marin <alejandrober96@gmail.com>
2024-04-01 11:05:46 -0500

    Add removeItemToCart method

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -39,3 +39,28 @@ exports.addItemToCart = async (req, res) => {
     return res.status(500).json({ error: error, message: 'Error saving the item' });
   }
 };
+
+exports.removeItemFromCart = async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ user: req.user._id });
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    const productToRemove = req.params.productId;
+    const itemIndex = cart.cartItems.findIndex(item => item.product.toString() === productToRemove.toString());
+
+    if (itemIndex === -1) {
+      return res.status(404).json({ message: 'Item not found in cart' });
+    }
+
+    cart.cartItems.splice(itemIndex, 1);
+
+    const updatedCart = await cart.save();
+    return res.status(200).json({ updatedCart });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error, message: 'Error removing item from cart' });
+  }
+};
