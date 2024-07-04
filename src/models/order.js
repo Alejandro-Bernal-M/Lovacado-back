e81568c9cd0d7c0bc1040a5dca11a6be40b17470
alejandro bernal marin <alejandrober96@gmail.com>
2024-07-03 19:59:49 -0500

    refactor: Add required fields to address in Order model

--- src/models/order.js
+++ src/models/order.js
@@ -4,13 +4,37 @@ const OrderSchema = mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true,
   },
   address: {
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    line1: {
+      type: String,
+      required: true,
+    },
+    line2: {
+      type: String,
+    },
+    postal_code: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+  },
+  email: {
     type: String,
     required: true,
   },
-  phone: {
+  name: {
     type: String,
     required: true,
   },
