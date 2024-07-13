e1e9f36c31127f86ce3762eb7ce98bf786054041
alejandro bernal marin <alejandrober96@gmail.com>
2024-07-12 19:05:32 -0500

    refactor: Update multer destination and filename in homeSection.js

--- src/routes/homeSection.js
+++ src/routes/homeSection.js
@@ -1,17 +1,19 @@
 const multer = require('multer');
 const express = require('express');
+const path = require('path');
+const shortid = require('shortid');
 const router = express.Router();
 const { createHomeSection, getHomeSections, updateHomeSection, deleteHomeSection } = require('../controllers/homeSection');
 const { requireSignin, requireAdmin } = require('../common-middlewares');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, path.join(path.dirname(__dirname), 'uploads'))
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, shortid.generate() + '-' + file.originalname)
   }
-});
+})
 
 const upload = multer({ storage: storage });
 
